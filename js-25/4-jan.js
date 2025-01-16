89d0e99fec08ea0da8ba65e4fd97908c15736add
nikhilgupta1699 <nikhil2nikhil28@gmail.com>
2025-01-17 00:53:58 +0530

    commit

---
+++ js-25/4-jan.js
@@ -0,0 +1,13 @@
+document.write("<center><h3>this is js code</h3></center>")
+document.write("<center><h2>ANother example</h2></center>")
+
+let num1=20
+let num2=35
+let res=num1+num2
+document.write(`<h1> <span style="color:red">${num1}</span>+${num2}=${res}</h1>`)
+console.log(res)
+
+let n1=Number(prompt("enter a num1:"))
+let n2=Number(prompt("enter a num2:"))
+document.write(n1+n2)
+console.log(n1+n2)
