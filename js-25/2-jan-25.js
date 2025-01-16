fd6e4ac1b38173f1a6283590892cdd3eb841d5f7
nikhilgupta1699 <nikhil2nikhil28@gmail.com>
2025-01-16 20:14:34 +0530

    first commit

---
+++ js-25/2-jan-25.js
@@ -0,0 +1,8 @@
+var name1=prompt("enter your name:")
+//alert("this is a js code")
+document.write(name1)
+document.write("<br>")
+document.write("<hr>")
+document.write("<h1>This is a heading</h1>")
+
+console.log("this is only show ing console area")
